1d2343677f93e17ea3a188319501d76ca528aca9
gerardoprada <gpradag@msn.com>
2018-02-25 01:13:05 -0800

    Update dialog

--- app.js
+++ app.js
@@ -20,22 +20,20 @@ var inMemoryStorage = new builder.MemoryBotStorage();
 
 var bot = new builder.UniversalBot(connector, [
     function (session) {
-        session.send("Yo I'm Fyve Bot.");
+        session.send("Hi, I'm FyveBot. I can find and summarize data about any topic.");
         builder.Prompts.text(session, "What would you like to learn about?");
     },
     function (session, results) {
         session.dialogData.topic = results.response;
-        session.send("K Ill teach u about %s", session.dialogData.topic);
-        session.send("Here's your learning bruh");
-        session.sendTyping();
-        builder.Prompts.choice(session, "Did you like the content?", "yes|no", {listStyle: 2});
+        session.send("Awesome, Ill look up information about %s", session.dialogData.topic);
+        builder.Prompts.choice(session, "Would you like me to search text or video sources?", "text|video", {listStyle: 2});
     },
     function (session, results) {
-        if (results.response.entity === "no") {
-            session.send("Too bad");
-        } else {
-            session.send("K bye");
-        }
+        session.dialogData.format = results.response.entity;
+        session.send("Great, Ill look up %s about %s", session.dialogData.format, session.dialogData.topic);
+        // TODO: GET STUFF FROM SERVER
+        session.sendTyping();
+        session.send("I hope that was useful. Bye!");
         session.endDialog();
     }
 ]).set('storage', inMemoryStorage);
